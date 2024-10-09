4633c9fea34b6e1c87ab39978be9e4d602e1a83c
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2024-10-09 12:58:53 +0200

    adding documentation for functions

--- animals/animal.go
+++ animals/animal.go
@@ -1,8 +1,15 @@
 package animals
 
+// Animal is an interface that defines the behavior of an animal.
+// It's used to demonstrate how interfaces work in Go during the workshop.
+// I completely understand that this is a contrived example and that animals in real life don't have a Display method.
 type Animal interface {
 	Display()
 }
+
+// ShowAnimal prints the underlying data of the animal to stdout.
+// Intentionally not a method with receiver, but maybe in a future update?
+//   - animal - the animal to display.
 
 func ShowAnimal(animal Animal) {
 	animal.Display()
--- animals/dog.go
+++ animals/dog.go
@@ -6,12 +6,18 @@ import (
 	"log/slog"
 )
 
+// Dog is a representation of a dog and contains a name, age, and breed.
+// Don't use this type directly, use NewDog instead.
 type Dog struct {
 	Name     string `json:"name"`
 	Age      int    `json:"age"`
 	DogBreed `json:"breed"`
 }
 
+// NewDog creates a new instance of Dog with the specified values.
+//   - name - a unique name for this dog; common names include 'fido' and 'max'.
+//   - age - the current age of the dog. Must be greater than zero.
+//   - breed - the breed of the dog. Use the DogBreed constants to specify the breed.
 func NewDog(name string, age int, breed DogBreed) (*Dog, error) {
 	if age <= 0 {
 		return nil, errors.New("age must be greater than zero")
@@ -26,10 +32,14 @@ func NewDog(name string, age int, breed DogBreed) (*Dog, error) {
 	return &dog, nil
 }
 
+// Display prints the dog's information in a nice format. It's printed to stdout.
 func (d *Dog) Display() {
 	fmt.Printf("Dog\n Name:%s\n Age:%d\n Breed:%s\n", d.Name, d.Age, d.DogBreed)
 }
 
+// SetAge is a setter method that updates the dog's age.
+// This is used in the workshop portion that discusses pointers.
+//   - age - the new age of the dog. Must be greater than zero.
 func (d *Dog) SetAge(age int) {
 	d.Age = age
 }
