2bd60d91853fc21ecd0480c11697f369fd21c13b
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2024-10-09 15:25:06 +0200

    adding dev container and more comments

--- animals/animal.go
+++ animals/animal.go
@@ -3,6 +3,12 @@
 // I'm so sorry this is basic and contrived. I'm not a zoologist.
 package animals
 
+// This is not used at all. I just put this here so the docs don't feel lonely
+const IS_ANIMAL = true
+
+// This looks impressive, it does nothing
+var _ Animal = (*Dog)(nil)
+
 // Animal is an interface that defines the behavior of an animal.
 // It's used to demonstrate how interfaces work in Go during the workshop.
 // I completely understand that this is a contrived example and that animals in real life don't have a Display method.
