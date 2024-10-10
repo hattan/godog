e7e72ae14686f2cecafc68311de8007162564d21
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2024-10-10 11:29:56 +0200

    new implementation

--- animals/dogbreed.go
+++ animals/dogbreed.go
@@ -23,17 +23,14 @@ var Breed_value = map[string]DogBreed{
 	"mutt":     Mutt,
 }
 
-// Converts a DogBreed type to a string. Used for JSON marshalling and logging
 func (breed DogBreed) String() string {
 	key, ok := getBreedKeyByValue(Breed_value, breed)
 	if !ok {
 		slog.Error(fmt.Sprintf("Error DogBreedToString. %d is not a valid breed", breed))
-		return "unknown"
 	}
 	return key
 }
 
-// Returns a breed name from a DogBreed value by utilizing the built-in map
 func getBreedKeyByValue(m map[string]DogBreed, value DogBreed) (key string, ok bool) {
 	for k, v := range m {
 		if v == value {
@@ -45,17 +42,14 @@ func getBreedKeyByValue(m map[string]DogBreed, value DogBreed) (key string, ok b
 	return
 }
 
-// IsMutt returns true if the DogBreed is Mutt
 func (breed DogBreed) IsMutt() bool {
 	return breed == Mutt
 }
 
-// MarshalJSON marshals the DogBreed to a JSON string
 func (breed DogBreed) MarshalJSON() ([]byte, error) {
 	return json.Marshal(breed.String())
 }
 
-// UnmarshalJSON unmarshals a JSON string to a DogBreed
 func (s *DogBreed) UnmarshalJSON(data []byte) (err error) {
 	var breed string
 	if err := json.Unmarshal(data, &breed); err != nil {
@@ -67,7 +61,6 @@ func (s *DogBreed) UnmarshalJSON(data []byte) (err error) {
 	return nil
 }
 
-// FromString converts a string to a DogBreed
 func FromString(s string) (DogBreed, error) {
 	s = strings.TrimSpace(strings.ToLower(s))
 	value, ok := Breed_value[s]
